69a60ea2d74df4619c490224492f31a28bf55c77
sieversJonas <jonas.sievers@kit.edu>
2023-11-16 14:42:53 +0100

    Added comments and restructured utils

--- src/Environment.py
+++ src/Environment.py
@@ -11,9 +11,7 @@ from tf_agents.trajectories import time_step as ts
 
 class Environment(py_environment.PyEnvironment):
 
-    def __init__(self, data, init_charge, max_timeslots=48, max_days=365, capacity=13.5,
-                 power_battery=4.6, power_grid=25.0, test=False):
-        """
+    """
     Initialize the environment. Default values simulate tesla's powerwall
 
     :param data: load, pv and electricity price data
@@ -25,28 +23,38 @@ class Environment(py_environment.PyEnvironment):
     :param power_grid: power of the electricity grid in kW
     :param test: activates test logs
     """
-
-        self._current_timeslot = -1
-        self._max_timeslots = max_timeslots
-        self._current_day = 0
-        self._max_days = max_days
-        self._capacity = capacity
-        self._power_battery = power_battery
-        self._init_charge = init_charge
-        self._soe = np.float32(init_charge)
-        self._power_grid = power_grid
-        self._episode_ended = False
-        self._electricity_cost = 0.0
-        self._test = test
-        self._test_writer = None
+    def __init__(
+            self, 
+            data, #load, pv and electricity price data
+            init_charge, #initial state of charge of the battery in kWh
+            max_timeslots=48, #count of timeslots in 24h interval
+            max_days=365, #count of days to train on
+            capacity=13.5, #capacity of the battery in kWh
+            power_battery=4.6, #power of the battery in kW
+            power_grid=25.0, #power of the electricity grid in kW
+            test=False
+        ):
+ 
+        self._current_timeslot = -1 #Tracks the current timeslot in the simulation.
+        self._max_timeslots = max_timeslots #Maximum number of timeslots in a 24-hour interval.
+        self._current_day = 0 #Tracks the current day in the simulation.
+        self._max_days = max_days #Maximum number of days to train on.
+        self._capacity = capacity #Capacity of the battery.
+        self._power_battery = power_battery # Power of the battery.
+        self._init_charge = init_charge #Initial state of charge of the battery.
+        self._soe = np.float32(init_charge) #State of charge of the battery (initialized based on init_charge).
+        self._power_grid = power_grid #Power of the electricity grid.
+        self._episode_ended = False #Boolean flag indicating whether the current episode has ended.
+        self._electricity_cost = 0.0 #Cumulative electricity cost incurred during the simulation.
+        self._test = test #Boolean flag indicating whether the environment is in test mode.
+        self._test_writer = None #TensorFlow summary writer used for test logs.
 
         if self._test:
             self._test_writer = tf.compat.v2.summary.create_file_writer(
                 logdir='./log/test/ddpg', flush_millis=10000
             )
 
-        # Continuous action space [grid]
-        # grid: -2.3 - 12,5 kW
+        # Continuous action space [grid] - grid bounded: -2.3 - 12,5 kW
         self._action_spec = array_spec.BoundedArraySpec(shape=(1,), dtype=np.float32, minimum=-self._power_battery / 2,
                                                         maximum=self._power_grid / 2, name='action')
 
@@ -54,48 +62,66 @@ class Environment(py_environment.PyEnvironment):
         self._observation_spec = array_spec.BoundedArraySpec(shape=(4,), dtype=np.float32, minimum=0.0,
                                                              name='observation')
 
+        #Hold the load, PV, and electricity price data, respectively, passed during initialization.
         self._load_data, self._pv_data, self._electricity_prices = data
 
+    """
+    Return the action spec
+    :return: action spec
+    """
     def action_spec(self):
-        """
-        Return the action spec
-        :return: action spec
-        """
+
         return self._action_spec
 
+    """
+    Return the oberservation spec
+    :return: oberservation spec
+    """
     def observation_spec(self):
-        """
-        Return the oberservation spec
-        :return: oberservation spec
-        """
+
         return self._observation_spec
 
+    """
+    Reset the environment, preparing it for a new episode.
+    :return: initial TimeStep
+    """
     def _reset(self):
-        """
-        Reset the environment
-        :return: initial TimeStep
-        """
-        self._current_timeslot = -1
-        self._current_day = 0
-        self._soe = self._init_charge
-        self._episode_ended = False
-        self._electricity_cost = 0.0
-        return ts.restart(
-            np.array([self._init_charge, 0.0, 0.0, 0.0], dtype=np.float32))
 
+        self._current_timeslot = -1 #is set to -1, indicating that the next timeslot will be the first one.
+        self._current_day = 0 # is set to 0, indicating the start of a new training day.
+        self._soe = self._init_charge #is set to the initial charge value (_init_charge).
+        self._episode_ended = False #signaling the start of a new episode.
+        self._electricity_cost = 0.0 #as it accumulates the electricity cost during each episode.
+        
+        #The method returns an initial TimeStep object using the ts.restart function. [SoE, load, pv, price]
+        return ts.restart(
+            np.array([self._init_charge, 0.0, 0.0, 0.0], dtype=np.float32)
+            )
+
+    """
+    The _step method simulates the agent's action in the environment, 
+    updates the state based on the action taken, and returns the next TimeStep object, 
+    which encapsulates the new state, reward, and whether the episode has ended.
+    :param action: action taken by the policy
+    :return: next TimeStep
+    """
     def _step(self, action):
+
         """
-        Take the action in the environment
-        :param action: action taken by the policy
-        :return: next TimeStep
+        Update Timeslot:
+        We first update the current timeslot and day in the simulation.
+        If the current timeslot exceeds the maximum allowed timeslots for a day, 
+        it increments the current day and resets the timeslot to the first one. This simulates the transition to a new day.
         """
-        # Update timeslot
         if self._current_timeslot >= self._max_timeslots - 1:
             self._current_day += 1
             self._current_timeslot = 0
         else:
             self._current_timeslot += 1
 
+        """
+        If the episode has already ended (_episode_ended is True), the environment is reset to its initial state by calling the reset method.
+        """
         if self._episode_ended:
             return self.reset()
 
@@ -116,12 +142,16 @@ class Environment(py_environment.PyEnvironment):
                                                                                                            action_taken)
         self._soe = new_soe
 
-        # energy bought from grid in Wh
-        cost = energy_from_grid * electricity_price
+        # Calculate Costs and Profits
+        cost = energy_from_grid * electricity_price #energy bought from grid in Wh
         profit = energy_feed_in * electricity_price * 0.7
         self._electricity_cost += cost - profit
 
         # Battery wear model
+        """
+        A battery wear cost is calculated based on the current state of charge. 
+        If the state of charge is below 20% or above 80% of the battery capacity, wear costs are incurred.
+        """
         if new_soe < (0.2 * self._capacity):
             battery_wear_cost = (0.2 * self._capacity) - new_soe
         elif new_soe > (0.8 * self._capacity):
@@ -143,16 +173,27 @@ class Environment(py_environment.PyEnvironment):
                 tf.summary.scalar(name='pv', data=pv)
                 tf.summary.scalar(name='load', data=load)
 
-        # Last timeslot returns end of data information
+        """
+        If the last timeslot is reached and the maximum number of training days has been reached, 
+        the episode is marked as ended. The method returns a termination TimeStep object.
+        """
         if self._current_day >= self._max_days - 1 and self._current_timeslot >= self._max_timeslots - 1:
             self._episode_ended = True
             if self._test:
                 with self._test_writer.as_default(step=(self._current_day * 48) + self._current_timeslot):
                     tf.summary.scalar(name='cost', data=self._electricity_cost)
+            
             return ts.termination(np.array([new_soe, load, pv, electricity_price], dtype=np.float32),
                                   reward=-current_reward)
+        
+        """
+        For regular time steps, a transition TimeStep object is returned, representing the new state, 
+        the negative of the current reward (as the environment aims to minimize costs), 
+        and a discount factor of 1.0 (indicating no discounting in this scenario).
+        """
         return ts.transition(np.array([new_soe, load, pv, electricity_price], dtype=np.float32), reward=-current_reward,
                              discount=1.0)
+
 
     def chargeBattery(self, amount_to_charge, old_soe, load, pv, action_taken):
         """
