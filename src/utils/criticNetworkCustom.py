ffb86e58d7ce9477b855a03c90e828b4d61e7c01
sieversJonas <jonas.sievers@kit.edu>
2023-11-24 17:38:45 +0100

    Custom Critic networks

--- src/utils/criticNetworkCustom.py
+++ src/utils/criticNetworkCustom.py
@@ -12,6 +12,70 @@ class CriticNetworkCustom(network.Network):
     def __init__(self,
                 observation_spec,
                 action_spec,
+                custom_layers=None,
+                name='CriticNetworkCustom'):
+        # Invoke constructor of network.Network
+        super(CriticNetworkCustom, self).__init__(
+              input_tensor_spec=(observation_spec, action_spec), state_spec=(), name=name)
+
+        self._obs_spec = observation_spec
+        self._action_spec = action_spec
+
+        # Initialize the custom Keras layers provided:
+        self._custom_layers = custom_layers
+        if custom_layers is not None:
+            # When custom layers are provided, use Concatenate as preprocessing_combiner
+            self._encoder = encoding_network.EncodingNetwork(
+                (observation_spec, action_spec),
+                fc_layer_params=(400,),
+                preprocessing_combiner=tf.keras.layers.Concatenate(axis=-1),
+                activation_fn=tf.keras.activations.relu,
+                kernel_initializer=tf.keras.initializers.VarianceScaling(scale=1./3., mode='fan_in', distribution='uniform'),
+                batch_squash=True)
+        else:
+            self._encoder = encoding_network.EncodingNetwork(
+                (observation_spec, action_spec),
+                fc_layer_params=(400,),
+                activation_fn=tf.keras.activations.relu,
+                kernel_initializer=tf.keras.initializers.VarianceScaling(scale=1./3., mode='fan_in', distribution='uniform'),
+                batch_squash=True)
+
+        # Initialize the custom tf layers here:
+        self._dense1 = tf.keras.layers.Dense(400, name='Dense1')
+        self._value_layer = tf.keras.layers.Dense(1,
+                                                  activation=tf.keras.activations.linear,
+                                                  name='Value')  # Q-function output
+
+    def call(self, observations, step_type=(), network_state=()):
+        # Forward pass through the custom Keras layers provided:
+        state, network_state = self._encoder(observations,
+                                             step_type=step_type,
+                                             network_state=network_state)
+
+        if self._custom_layers is not None:
+            # Additional logic for custom layers
+            layer_outputs = [state]
+            for layer in self._custom_layers:
+                state = layer(state)
+                layer_outputs.append(state)
+
+            value = self._value_layer(state)
+
+        else:
+            # Forward pass without custom layers
+            state = self._dense1(state)
+            value = self._value_layer(state)
+
+        return tf.reshape(value, [-1]), network_state
+
+
+
+"""class CriticNetworkCustom(network.Network):
+
+    def __init__(self,
+                observation_spec,
+                action_spec,
+                custom_layers=[tf.keras.layers.Dense(units=16, activation='relu'),tf.keras.layers.Dense(units=16, activation='relu')],
                 name='CriticNetworkCustom'):
         # Invoke constructor of network.Network
         super(CriticNetworkCustom, self).__init__(
@@ -46,4 +110,4 @@ class CriticNetworkCustom(network.Network):
         state = self._dense1(state)
         value = self._value_layer(state)
 
-        return tf.reshape(value,[-1]), network_state
+        return tf.reshape(value,[-1]), network_state"""
