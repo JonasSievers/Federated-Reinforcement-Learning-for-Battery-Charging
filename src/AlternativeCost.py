15df7fc65e36bf0c82bbc8017783cc9dcc650a55
Pascal Henrich <pascalh.1505@gmail.com>
2023-12-08 18:06:19 +0100

    added baseline

--- src/AlternativeCost.py
+++ src/AlternativeCost.py
@@ -2,14 +2,14 @@ import numpy as np
 
 import sys
 sys.path.insert(0, '..')
-import utils.Dataloader as Dataloader
+import utils.dataloader as Dataloader
 
 """
 Calculates the alternative cost in the case without a battery
 """
 for i in range(13,14):
-    load_data, pv_data, price_data = Dataloader.get_customer_data(Dataloader.loadData('../data/load1213.csv'),
-                                             Dataloader.loadPrice('../data/price.csv'), i)
+    load_data, pv_data, price_data = Dataloader.get_customer_data(Dataloader.loadData('data/load1213.csv'),
+                                             Dataloader.loadPrice('data/price.csv'), i)
     day = 0
     timeslot = 0
     cost = 0.0
@@ -24,6 +24,7 @@ for i in range(13,14):
         if net_load < 0:
             cost += net_load * electricity_price * 0.7
         else:
+            print(net_load)
             cost += np.clip(a=net_load, a_min=0, a_max= 100.0) * electricity_price
         sum_load += load
         sum_pv += pv
