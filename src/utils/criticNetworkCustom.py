c97294fd10eade962a3d4c85c6a0cc52a16323ee
sieversJonas <jonas.sievers@kit.edu>
2023-12-11 12:33:53 +0100

    Custom actor critic networks

--- src/utils/criticNetworkCustom.py
+++ src/utils/criticNetworkCustom.py
@@ -9,20 +9,24 @@ from tf_agents.networks import encoding_network
 
 class CriticNetworkCustom(network.Network):
 
-    def __init__(self, observation_spec, action_spec, custom_layers, name='CriticNetworkCustom'):
+    def __init__(self, 
+                 observation_spec, 
+                 action_spec, 
+                 custom_layers=[tf.keras.layers.Dense(units=16, activation='relu'),tf.keras.layers.Dense(units=16, activation='relu')], 
+                 name='CriticNetworkCustom',
+                 use_ensemble=False
+                 ):
         # Invoke constructor of network.Network
-        super(CriticNetworkCustom, self).__init__(
-              input_tensor_spec=(observation_spec, action_spec), state_spec=(), name=name)
+        super(CriticNetworkCustom, self).__init__(input_tensor_spec=(observation_spec, action_spec), state_spec=(), name=name)
 
         self._obs_spec = observation_spec
         self._action_spec = action_spec
-        
-        print("observation_spec: ", observation_spec) #shape=(4,)
-        print("action_spec: ", action_spec) #shape=(1,)
-        
+
+        #flat_action_spec = tf.nest.flatten(action_spec)
+        #self._single_action_spec = flat_action_spec[0]
+                
         # Encoding layer concatenates state and action inputs, adds dense layer:
-        kernel_initializer = tf.keras.initializers.VarianceScaling(
-            scale=1./3., mode='fan_in', distribution='uniform')
+        kernel_initializer = tf.keras.initializers.VarianceScaling(scale=1./3., mode='fan_in', distribution='uniform')
         combiner = tf.keras.layers.Concatenate(axis=-1)
         self._encoder = encoding_network.EncodingNetwork(
             (observation_spec, action_spec),
@@ -34,33 +38,32 @@ class CriticNetworkCustom(network.Network):
 
         # Initialize the custom tf layers here:
         self._custom_layers = custom_layers
-        
-        # Add custom layers dynamically
-        for layer in self._custom_layers:
-            setattr(self, f"_custom_{layer.name}", layer)
 
-        # Initialize the value layer
-        self._value_layer = tf.keras.layers.Dense(1,
-                                                  activation=tf.keras.activations.linear,
-                                                  name='Value')  # Q-function output
+        # Optional - TO BE IMPLEMENTED CORRECTLY: Use ensemble mode
+        self._use_ensemble = use_ensemble
+        if self._use_ensemble:
+            self._ensemble_layer = tf.keras.layers.Concatenate(axis=-1, name='ensemble_layer')
+        
+          # Initialize the value layer -> output_dim = 1 (Q-Value)
+        self._value_layer = tf.keras.layers.Dense(
+            units= 1,
+            activation=tf.keras.activations.linear,
+            kernel_initializer=tf.keras.initializers.RandomUniform(minval=-0.003, maxval=0.003),
+            name='Value')  # Q-function output
 
 
     def call(self, observations, step_type=(), network_state=()):
         # Forward pass through the custom tf layers here (defined above):
-        state, network_state = self._encoder(observations, 
-                                             step_type=step_type, 
-                                             network_state=network_state)
-        
-        print("state: ", state.shape) #shape=(1, 400)
-            
+        state, network_state = self._encoder(observations, step_type=step_type, network_state=network_state)
+                          
         # Apply custom layers
+        layer_outputs = []
         for layer in self._custom_layers:
-            print(f"Custom Layer {layer.name} Input Shape:", state.shape)
-            state = getattr(self, f"_custom_{layer.name}")(state)
-            print(f"Custom Layer {layer.name} Output Shape:", state.shape)
+            layer_outputs.append(state)
+        # Additional logic for ensemble mode, if needed
+        if self._use_ensemble:
+            state = self._ensemble_layer(layer_outputs)
 
-        print("Own layers done")
         value = self._value_layer(state)
-        print("value: ", value) #shape=(1, 1)
-
+    
         return tf.reshape(value, [-1]), network_state
