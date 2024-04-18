4aae68812e9f848fcbd29d7d1aa9266b138c2ffe
Pascal Henrich <pascalh.1505@gmail.com>
2024-04-18 15:37:00 +0200

    updated baseline

--- src/Baseline.py
+++ src/Baseline.py
@@ -12,32 +12,25 @@ class Baseline:
         self.feed_in_price = feed_in_price
         self.fixed_battery_action = 0.0
         self.electricity_cost = 0.0
-
-    def forecast_mean_fn(self, current_price, price_forecast):
-        if current_price > price_forecast:
-            return (- self.fixed_battery_action)
-        elif current_price < price_forecast:
-            return self.fixed_battery_action
-        else:
-            return 0.0
     
-    def percentileOverPastTimeslots(self, current_price, price_past):
-        if price_past.empty:
+    def ruleBasedCharching(self, current_price, price, pv):
+        if price.empty:
             return 0.0
-        low_price = np.percentile(price_past.squeeze(), 20)
-        high_price = np.percentile(price_past.squeeze(), 80)
+        
+        low_price = np.percentile(price.squeeze(), 20)
 
-        if current_price > high_price:
-            return (- self.fixed_battery_action)
-        elif current_price < low_price:
+        # Strom sehr billig über die nächsten 24h oder PV Erzeugung
+        if (current_price <= low_price) | (pv > 0):
             return self.fixed_battery_action
-        else:
-            return 0.0
+        # Keine PV Erzeugung
+        if (pv <= 0):
+            return (- self.fixed_battery_action)
+        return 0.0
 
-    
-    def main(self, fixed_battery_action, action_method):
-        # Reset cost
+    def main(self, fixed_battery_action):
+        # Reset
         self.electricity_cost = 0.0
+        self.battery_soe = 0.0
 
         self.fixed_battery_action = fixed_battery_action 
     
@@ -46,16 +39,17 @@ class Baseline:
             p_load =  self.test["load_"+self.customer].loc[timeslot]
             p_pv =  self.test["pv_"+self.customer].loc[timeslot]
             price_buy = self.test["price"].loc[timeslot]
-            # price_forecast = self.test["price"].loc[timeslot+1:timeslot+7].mean()
-            price_past = self.test["price"].loc[timeslot-(48*7):timeslot-1]
+            price_forecast = self.test["price"].loc[timeslot:timeslot+48]
 
             # Decide whether charge or discharge on price forecast
-            battery_action = action_method(price_buy,price_past)
+            battery_action = self.ruleBasedCharching(price_buy,price_forecast,p_pv)
 
             # Calculate new SOE
             old_soe = self.battery_soe
             self.battery_soe = np.clip(old_soe + battery_action, a_min=0.0, a_max=self.battery_capacity, dtype=np.float32)
             p_battery = old_soe - self.battery_soe
+
+            p_battery_left = self.battery_power - np.abs(p_battery)
     
             
             # Initialize values
@@ -68,7 +62,11 @@ class Baseline:
                 energy_from_grid = energy_management
             # Feed in remaining energy
             else:
-                energy_feed_in = np.abs(energy_management)
+                charge_battery = np.clip(abs(energy_management),a_min=0.0, a_max=min(p_battery_left,self.battery_capacity-self.battery_soe))
+                leftover_energy = abs(energy_management)-charge_battery
+                self.battery_soe = np.clip(old_soe + charge_battery, a_min=0.0, a_max=self.battery_capacity, dtype=np.float32)
+                energy_feed_in = leftover_energy
+
 
             cost = energy_from_grid * price_buy
             profit = energy_feed_in * self.feed_in_price
@@ -78,5 +76,5 @@ class Baseline:
 
 
 baseline = Baseline()
-for battery_action in np.arange(0.1,2.4,0.1):
-    baseline.main(battery_action, baseline.percentileOverPastTimeslots)
+# for battery_action in np.arange(0.1,2.4,0.1):
+baseline.main(0.2)
